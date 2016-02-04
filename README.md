b2d7ad4985a2d0dcc11a0c015300218df6b32319
sxnwlfkk <saxonwolfkok@gmail.com>
2016-02-04 22:30:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Python web scraping
+
+The main goal of this project to build a crawler/scraper, which can navigate
+treelike webbooks, and extract html text for further processing with tools
+like Calibre.
---
+++ epub_scrape.py
@@ -0,0 +1,7 @@
+# This program scrapes a tree structured webbook, and parses it to a single file
+
+# Load page and crawl it
+
+# Ask user which link he wants to scrape
+
+# Scrape the pages to a single html file for further processings.
