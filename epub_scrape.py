c404eb73218c61343e501d5a95c341a4714d512e
sxnwlfkk <saxonwolfkok@gmail.com>
2016-02-05 11:37:13 +0100

    Working on page sanitization

--- epub_scrape.py
+++ epub_scrape.py
@@ -1,26 +1,17 @@
 # This program scrapes a tree structured webbook, and parses it to a single file
 
+import os
+
+
 # Load page and crawl it
 
 # Ask user which link he wants to scrape
 
-# Scrape the pages to a single html file for further processings.
+# Scrape the pages to a single html file for further processing.
 
-# Remove <a> tags
-
-text = """<p id="a_zen_master_st">A <a id="I_indexterm2_d1e919" class="indexterm"></a>Zen master of great <a id="I_indexterm2_d1e923" class="indexterm"></a>renown was visited by a young philosopher who had traveled
-    from a distant land to meet him. The master agreed to see him because the
-    philosopher came with high recommendations by his teachers. The two sat
-    under a tree to converse and the subject hastily turned to what the master
-    could teach the young philosopher. Recognizing the flame of youth, the
-    master smiled warmly and started to describe his meditation techniques. He
-    was cut short by the philosopher, who said: “Yes, I understand what you
-    are talking about! We did a similar technique at the temple, but instead
-    we used images to focus!”</>"""
+# Remove <any> tags
 
 def rem_tag(text, start, end):
-    # These need deleting:
-    # <a ... > and </a>
     start_i = 0
     end_i = 0
     start_i = text.find(start)
@@ -37,5 +28,27 @@ def rem_all_tag(text, start, end):
         atext, find = rem_tag(atext, start, end)
     return atext
 
-atext = rem_all_tag(text, '<a', 'a>')
-print(atext)
+def ret_in_tag(text, start, end, start_pos):
+    end_pos = text.find(end, start_pos)
+    o_text = text[start_pos:] + text[:end_pos+2]
+    return o_text
+
+def extract_paragraph(page):
+    in_text = page[page.find('<body>'):]
+    out_text = ''
+    for i in range(len(in_text)):
+        if page[i] == '<':
+            if page[i+1] == 'p' and page[i+2] == ' ':
+                out_text += ret_in_tag(in_text, '<p', 'p>', i)
+    return out_text
+
+def test():
+    inpath = 'test_page.html'
+    outpath = 'test_out_page.html'
+    with open(inpath, 'r') as page_source:
+        page = page_source.read()
+    with open(outpath, 'w') as outfile:
+        out_source = extract_paragraph(page)
+        # out_source = rem_all_tag(out_source, '<a', 'a>')
+        outfile.write(out_source)
+    print('Done.')
