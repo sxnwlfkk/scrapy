e3241369924bbf17a4ed119f10a706a4d14c3007
sxnwlfkk <saxonwolfkok@gmail.com>
2016-02-06 21:09:31 +0100

    Added stubs to webook_class

--- webook_class.py
+++ webook_class.py
@@ -6,5 +6,23 @@ import os
 
 class Webook():
 
-    def __init__(url):
+    def __init__(self, url, path):
         self.url = url
+        if path != '':
+            self.path = path
+            self.text = self._import()
+
+    def print_links(self):
+        pass
+
+    def sort_links(self, unwanted):
+        pass
+
+    def remove_tags(self, tags):
+        pass
+
+    def export(self, output):
+        pass
+
+    def _import(self):
+        pass
--- webook_main.py
+++ webook_main.py
@@ -7,14 +7,19 @@ from webook_class import Webook
 
 def main():
     """Docstring placeholder."""
-    # Insert the index of the page you want to download
+    # Insert the index of the page you want to download.
     # url = 'http://something.com/or/other/index.html'
+    # If you want to download from the internet, leave <path> empty.
+    # You can use and existing html file. In this case leave <url> empty
+    # and write the absolute path to file.
+    # path = '/home/user/file.html'
     url = ''
-    book = Webook(url)
+    path = ''
+    book = Webook(url, path)
 
     # Prints all the links on the index page. Uncomment it to determine wich
     # ones you don't need, then comment it after, for brevity.
-    book.print_links()
+    # book.print_links()
 
     # Write in the list below some unique marker of the links you don't want.
     unwanted = ['mailto', 'javascript']
